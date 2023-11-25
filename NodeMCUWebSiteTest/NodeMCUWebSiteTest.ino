aa24f41ae5044a980c8f2f4e22988057206b7934
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2023-11-25 11:47:34 +0200

    buttons with arrows

--- NodeMCUWebSiteTest/NodeMCUWebSiteTest.ino
+++ NodeMCUWebSiteTest/NodeMCUWebSiteTest.ino
@@ -2,8 +2,8 @@
 
 WiFiServer espServer(80);
 
-int RightMotorForwardVal; 
-int LeftMotorForwardVal;
+int RightMotor; 
+int LeftMotor;
 
 void setup()
 {
@@ -61,24 +61,30 @@ void loop()
   client.println("<head>");
   client.println("<meta charset='UTF-8'>");
   client.println("<meta name='viewport' content='width=device-width', initial-scale='1.0'>");
-  client.println("<title>Document</title>");
+  client.println("<title>NODE MCU Controller</title>");
+  client.println("<style>");
+  client.println("#InputButton {");
+  client.println("padding:0px;");
+  client.println("margin: 0px;");
+  client.println("font-size: 100px;");
+  client.println("width:  170px;");
+  client.println("height: 170px;");
+  client.println("}");
+  client.println("</style>");
   client.println("</head>");
   client.println("<body>");
-  client.println("<h4>Контрол на количка с ESP8266</h4>");
+  client.println("<center>");
+  client.println("<h4>Контролер на количка с ESP8266</h4>");
   client.println("<hr>");
   client.println("<br>");
-  client.println("<a id='LeftButton' href=\"/left\"><button>Left</button></a>");
-  client.println("<a href=\"/forward\"><button>Forward</button></a>");
-  client.println("<a id='BackwardButton' href=\"/backward\"><button>Backwards</button></a>");
-  client.println("<a id='RightButton' href=\"/right\"><button>Right</button></a>");
+  client.println("<a href=\"/left\"><button id='InputButton'>&lArr;</button></a>");
+  client.println("<a href=\"/forward\"><button id='InputButton'>&uArr;</button></a>");
+  client.println("<a href=\"/backward\"><button id='InputButton'>&dArr;</button></a>");
+  client.println("<a href=\"/right\"><button id='InputButton'>&rArr;</button></a>");
+  client.println("</center>");
   client.println("</body>");
   client.println("</html>");
-  client.println("");
-  client.println("");
-  client.println("");
-  client.println("");
-  
-
+  client.println("<hr>");
 
   client.stop();
   Serial.println("Client disconnected");
